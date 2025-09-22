99c67aa9df6fa4c449682587e88aec454a1f7da4
Sakinah Ali <sakinah.ali@proton.me>
2025-09-22 10:27:42 -0400

    optimized

--- Random Forest/#Random Forest.py
+++ Random Forest/#Random Forest.py
@@ -1,24 +1,25 @@
 #Random Forest
 #importing the libraries
-import pandas as pd
 from sklearn.datasets import load_iris
 
 #loading the dataset
 iris = load_iris()
+#print(dir(iris))
 
 #creating the dataframe
+import pandas as pd
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-df['target'] = iris.target
 df.head()
+df['target'] = iris.target
+df.head()   
 
 #splitting the dataset into features and target variable
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(df[iris.feature_names], df['target'], test_size=0.3)
-
-#adding random  forest classifier
+x_train, x_test, y_train, y_test = train_test_split(df.drop(['target'],axis='columns'),iris.target,test_size=0.2)
 from sklearn.ensemble import RandomForestClassifier
-model = RandomForestClassifier
+model = RandomForestClassifier(n_estimators=40)
 model.fit(x_train, y_train)
 
 #accuracy 
 print(model.score(x_test, y_test))
+
